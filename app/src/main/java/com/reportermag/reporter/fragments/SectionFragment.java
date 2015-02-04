efc92eca5941de76cd5920430fbe35de06921ee2
jrj2211 <jrj2211@RIT.edu>
2015-02-04 16:07:58 -0500

    Broke infinate scrolling, but transistioned to a ListView which should automatically recycle views as you scroll so images dont crash the app.

--- app/src/main/java/com/reportermag/reporter/fragments/SectionFragment.java
+++ app/src/main/java/com/reportermag/reporter/fragments/SectionFragment.java
@@ -15,10 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.reportermag.reporter.R;
 import com.reportermag.reporter.listeners.ArticleListener;
+import com.reportermag.reporter.util.ArticlesList;
 import com.reportermag.reporter.util.AsyncResponse;
 import com.reportermag.reporter.util.DownloadImageTask;
 import com.reportermag.reporter.util.ObservableScrollView;
@@ -28,20 +30,19 @@ import com.reportermag.reporter.util.ScrollViewListener;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class SectionFragment extends Fragment implements AsyncResponse, ScrollViewListener {
+public class SectionFragment extends Fragment implements AsyncResponse {
 
     private final String TAG = "SectionFragment";
-    private JSONArray json;
-    private LayoutInflater inflater;
-    private LinearLayout sectionContainer;
+    private ListView sectionContainer;
     private Integer sectionID;
     private LinearLayout titlebar;
     private Boolean loading = false;
-    private Integer lastNode = null;
+    private ArticlesList articles;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -72,137 +73,22 @@ public class SectionFragment extends Fragment implements AsyncResponse, ScrollVi
         colorFade.start();
 
         // Get the container
-        ObservableScrollView scrollContainer = (ObservableScrollView) inflater.inflate(R.layout.fragment_section, container, false);
-        scrollContainer.setScrollViewListener(this);
+        ListView sectionContainer = (ListView) inflater.inflate(R.layout.fragment_section, container, false);
 
-        this.inflater = inflater;
+        articles = new ArticlesList(getActivity(), new ArrayList<JSONObject>());
 
-        sectionContainer = (LinearLayout) scrollContainer.findViewById(R.id.section_articles);
+        sectionContainer.setAdapter(articles);
 
-        return scrollContainer;
+        return sectionContainer;
     }
 
     @Override
     public void processFinish(String result) {
 
         getActivity().findViewById(R.id.loading).setVisibility(View.GONE);
+        articles.loadJSON(result);
 
         // Unset loading
         loading = false;
-
-        try {
-            json = new JSONArray(result.trim());
-        } catch (Exception e) {
-            Log.e(TAG, "Could not parse articles JSON.");
-            return;
-        }
-
-        // Loop through results
-        for (int i = 0; i < json.length(); i++) {
-
-            JSONObject article;
-            Integer nodeID;
-
-            // Get the article object
-            try {
-                article = json.getJSONObject(i);
-                nodeID = article.getInt("nid");
-                lastNode = nodeID;
-            } catch (Exception e) {
-                Log.e(TAG, "Couldn't get article object.");
-                continue;
-            }
-
-            Integer nodeColor = Color.parseColor("#151515");
-
-            // Create article container
-            LinearLayout articleContainer = (LinearLayout) inflater.inflate(R.layout.article_abstract, sectionContainer, false);
-            sectionContainer.addView(articleContainer);
-
-            // Add the section circle
-            try {
-                String nodeSection = Character.toString(article.getString("section_name").charAt(0));
-                nodeColor = Color.parseColor(article.getString("section_color"));
-
-                TextView articleSection = (TextView) articleContainer.findViewById(R.id.abstract_section);
-
-                if (!nodeSection.isEmpty()) {
-                    articleSection.setText(nodeSection);
-                    GradientDrawable bgShape = (GradientDrawable) articleSection.getBackground();
-                    bgShape.setColor(nodeColor);
-                } else {
-                    articleSection.setVisibility(TextView.GONE);
-                }
-
-            } catch (Exception e) {
-                Log.e(TAG, "Could not get section for article id " + Integer.toString(nodeID));
-            }
-
-            // Add the Title
-            try {
-                TextView articleTitle = (TextView) articleContainer.findViewById(R.id.abstract_title);
-                articleTitle.setText(article.getString("title"));
-                articleTitle.setOnClickListener(new ArticleListener(nodeID, nodeColor, getActivity()));
-            } catch (Exception e) {
-                Log.e(TAG, "Could not get title for article id " + Integer.toString(nodeID));
-            }
-
-            // Add the byline
-            try {
-                JSONArray authors = article.getJSONArray("authors");
-                TextView articleByline = (TextView) articleContainer.findViewById(R.id.abstract_byline);
-                String author = authors.getJSONObject(0).getString("fullname");
-                articleByline.setText("By " + author + " on " + article.getString("date_format"));
-            } catch (Exception e) {
-                Log.e(TAG, "Could not get byline for article id " + Integer.toString(nodeID));
-            }
-
-            // Add the summary
-            try {
-                String nodeBody = Html.fromHtml(article.getString("body")).toString();
-                TextView articleSummary = (TextView) articleContainer.findViewById(R.id.abstract_summary);
-                articleSummary.setText(nodeBody);
-            } catch (Exception e) {
-                Log.e(TAG, "Could not get summary/trimmed for article id " + Integer.toString(nodeID));
-            }
-
-            // Add the Image
-            try {
-                String nodeImage = article.getString("imgLink");
-                if (!nodeImage.isEmpty() && !nodeImage.equals("[]")) {
-                    ImageView articleThumbnail = (ImageView) articleContainer.findViewById(R.id.abstract_image);
-                    articleThumbnail.setAdjustViewBounds(true);
-                    articleThumbnail.setVisibility(ImageView.VISIBLE);
-                    articleThumbnail.setOnClickListener(new ArticleListener(nodeID, nodeColor, getActivity()));
-
-                    // Download the image
-                    new DownloadImageTask(articleThumbnail).execute(nodeImage);
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Could not get image for article id " + Integer.toString(nodeID));
-            }
-        }
-    }
-
-    @Override
-    public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
-        // We take the last son in the scrollview
-        if (!loading) {
-            View view = scrollView.getChildAt(scrollView.getChildCount() - 1);
-
-            int diff = (view.getBottom() - (scrollView.getHeight() + scrollView.getScrollY()));
-            // if diff is zero, then the bottom has been reached
-            if (diff <= 500) {
-                loading = true;
-                loadMoreResults();
-            }
-        }
-    }
-
-    public void loadMoreResults() {
-        Log.w(TAG, "Loading more results after node " + lastNode);
-
-        PageContents downloadPage = new PageContents(this);
-        downloadPage.execute(getString(R.string.URL_SECTION) + "?s=" + sectionID.toString() + "&ln=" + lastNode);
     }
 }
